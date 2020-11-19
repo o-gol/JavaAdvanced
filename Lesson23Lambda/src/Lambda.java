a6f5bc1fe092b46862cf1a993d74def782d5211f
Голузов Олег Викторович <O.Goluzov@mtz.by>
2020-11-19 15:34:04 +0300

    lambda 19.11.2020

--- Lesson23Lambda/src/Lambda.java
+++ Lesson23Lambda/src/Lambda.java
@@ -1,11 +1,66 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Lambda {
     public static void main(String[] args) {
-        /*
+        List<Integer> integers=new ArrayList<>();
+        int[] ints=new int[10];
+        fillArr(ints);
+        fillList(integers);
+
+        System.out.println(integers);
+        System.out.println(Arrays.toString(ints) );
+
+        //map
+
+        ints=Arrays.stream(ints).map(operand -> operand*3).toArray();
+        integers=integers.stream().map(operand -> operand*3).collect(Collectors.toList());
+        System.out.println(integers);
+        System.out.println(Arrays.toString(ints) );
+
+        //filter
+        ints=Arrays.stream(ints).filter(operand -> operand%2==0).toArray();
+        integers=integers.stream().filter(operand -> operand%2==0).collect(Collectors.toList());
+        System.out.println(integers);
+        System.out.println(Arrays.toString(ints) );
+
+
+        //forEach
+
+        Arrays.stream(ints).forEach(System.out::println);
+        integers.stream().forEach(System.out::println);
+
+        // reduce
+
+        int a=Arrays.stream(ints).reduce(5,((left, right) -> left+right));
+        int b=integers.stream().reduce((left, right) -> left+right).get();
+        System.out.println(a);
+        System.out.println(b );
 
 
 
 
-         */
+
+
 
     }
+    private  static void fillList(List list){
+        for (int i = 0; i < 10; i++) {
+            list.add(i+1);
+        }
+
+    }
+
+    private  static void fillArr(int[] list){
+        for (int i = 0; i < 10; i++) {
+            list[i]=i+1;
+        }
+
+    }
+
+
+
 }
--- Lesson23Lambda/src/Program.java
+++ Lesson23Lambda/src/Program.java
@@ -1,29 +1,34 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
     interface Exempeleble{
-        int exemple();
+        int exemple(int i);
     }
 
     static class Exemple {
         List<Exempeleble> list=new ArrayList<>();
+        boolean boo=true;
 
         public void add(Exempeleble...args){
             for (int i = 0; i < args.length; i++) {
                 list.add(args[i]);
+                //System.out.println(args[i].exemple(6));
+                args[i].exemple(6);
 
             }
+
         }
 
         public int run(Exempeleble exempeleble){
-            return exempeleble.exemple();
+            return exempeleble.exemple(10);
         }
 
         public void runList(){
             for (Exempeleble h :
                     list) {
-                h.exemple();
+                h.exemple(6);
             }
 
         }
@@ -39,8 +44,8 @@ public class Program {
         }
 
         @Override
-        public int exemple() {
-            System.out.printf("Hello %s\n",id);
+        public int exemple(int x) {
+            System.out.printf("Hello %s-%s\n",id,x);
             return 1;
         }
     }
@@ -53,21 +58,21 @@ public class Program {
         }
 
         @Override
-        public int exemple() {
-            System.out.printf("Bay %s\n",id);
+        public int exemple(int x) {
+            System.out.printf("Bay %s-%s\n",id,x);
             return 1;
         }
     }
 
     public static void main(String[] args) {
         Exemple exemple=new Exemple();
-        int i= exemple.run(() -> {
+        int i= exemple.run(x -> {
             System.out.println("hello");
-             return 5;
+             return 15+x;
         });
         System.out.println(i);
 
-        i=exemple.run(()->7);
+        i=exemple.run(x->15+x);
 
         System.out.println(i);
 
@@ -78,7 +83,11 @@ public class Program {
         Bay bay2=new Bay();
 
         exemple.add(hello1,hello2,hello3,bay1,bay2);
-        exemple.runList();
+        //exemple.runList();
+
+        Exempeleble exempeleble=i1 ->{System.out.printf("Hello %s\n",5+i1);
+        return 5+i1;}; ;
+        exemple.add(exempeleble);
 
 
 
