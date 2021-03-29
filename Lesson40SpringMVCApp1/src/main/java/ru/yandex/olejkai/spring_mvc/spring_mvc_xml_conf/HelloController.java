7db93c5f6925010ee720a53fcf58f00a7912e1e5
Oleg <exemple@gmail.com>
2021-03-30 00:20:50 +0300

    Encoding problem 30.03.2021

--- Lesson40SpringMVCApp1/src/main/java/ru/yandex/olejkai/spring_mvc/spring_mvc_xml_conf/HelloController.java
+++ Lesson40SpringMVCApp1/src/main/java/ru/yandex/olejkai/spring_mvc/spring_mvc_xml_conf/HelloController.java
@@ -1,10 +1,14 @@
 package ru.yandex.olejkai.spring_mvc.spring_mvc_xml_conf;
 
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//@PropertySource(value = "classpath:config/data.properties", encoding = "UTF-8")
+//@ResponseBody()
 public class HelloController {
     @GetMapping("/hello_world")
     public String helloWorld(){
