b297f830febc17adc98975c4e610a3a721dbfbb0
oleg <oleg@email.com>
2021-03-30 15:37:41 +0300

    SpringMVCXML 30.03.21

--- Lesson40SpringMVCApp1/src/main/java/ru/yandex/olejkai/spring_mvc/spring_mvc_xml_conf/HelloController.java
+++ Lesson40SpringMVCApp1/src/main/java/ru/yandex/olejkai/spring_mvc/spring_mvc_xml_conf/HelloController.java
@@ -1,19 +1,43 @@
 package ru.yandex.olejkai.spring_mvc.spring_mvc_xml_conf;
 
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 //@PropertySource(value = "classpath:config/data.properties", encoding = "UTF-8")
 //@ResponseBody()
-public class HelloController {
-    @GetMapping("/hello_world")
-    public String helloWorld(){
+//@RequestMapping(value="/secure/example", headers="accept=text/html", method=RequestMethod.GET)
 
+public class HelloController {
+
+    /*@RequestMapping("/hello_world")
+    public ResponseEntity<String> handle() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(new MediaType("text", "plain"));
+        return new ResponseEntity<String>("Hello World", responseHeaders, HttpStatus.CREATED);
+    }*/
+
+
+    @GetMapping("/hello_world")
+    public String helloWorld(HttpServletResponse response) {
+        /*response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");*/
 //        return "index.html";
-        return "hello_world";
+        return "views/hello_world";
+    }
+    @GetMapping("/index")
+    public String helloWorld() {
+        /*response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");*/
+//        return "index.html";
+        return "views/my_view/index";
     }
 }
+
