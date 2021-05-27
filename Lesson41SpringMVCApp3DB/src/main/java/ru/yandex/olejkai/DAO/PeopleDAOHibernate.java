6cf7685dfae2900026c9603ed6145987b8b3599e
oleg <oleg@email.com>
2021-05-27 15:24:50 +0300

    hebernate\begin 27.05.21 w

---
+++ Lesson41SpringMVCApp3DB/src/main/java/ru/yandex/olejkai/DAO/PeopleDAOHibernate.java
@@ -0,0 +1,47 @@
+package ru.yandex.olejkai.DAO;
+
+import ru.yandex.olejkai.model.People;
+
+import java.util.List;
+
+public class PeopleDAOHibernate implements PeopleDAO {
+    @Override
+    public void addPeople(People people) {
+
+    }
+
+    @Override
+    public void deletePeople(People people) {
+
+    }
+
+    @Override
+    public People getPeople(People people) {
+        return null;
+    }
+
+    @Override
+    public People getPeopleByID(int id) {
+        return null;
+    }
+
+    @Override
+    public void deletePeopleByID(int id) {
+
+    }
+
+    @Override
+    public List<People> getAllPeople() {
+        return null;
+    }
+
+    @Override
+    public void updatePeople(People peopleForUpdate, People peopleFromUpdate) {
+
+    }
+
+    @Override
+    public int getMaxId() {
+        return 0;
+    }
+}
