7d1cb3a82ed3ef886ebe4c73dec694f95bcfa8d1
oleg <oleg@email.com>
2021-06-07 15:35:12 +0300

    hiber/encod w 07.06

--- Lesson41SpringMVCApp3DB/src/main/java/ru/yandex/olejkai/config/WebApp.java
+++ Lesson41SpringMVCApp3DB/src/main/java/ru/yandex/olejkai/config/WebApp.java
@@ -5,6 +5,7 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -40,6 +41,11 @@ public class WebApp extends AbstractAnnotationConfigDispatcherServletInitializer
     public void onStartup(ServletContext aServletContext) throws ServletException {
         super.onStartup(aServletContext);
         registerHiddenFieldFilter(aServletContext);
+
+        FilterRegistration.Dynamic encodingFilter = aServletContext.addFilter("encoding-filter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
     }
 
     /*@Override
