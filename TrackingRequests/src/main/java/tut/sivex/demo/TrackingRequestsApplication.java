65da4462f9156b1c8be8b468677dd5ad0d036420
oleg <oleg@email.com>
2021-06-14 14:10:07 +0300

    tracking_request_first_commite 14.06

---
+++ TrackingRequests/src/main/java/tut/sivex/demo/TrackingRequestsApplication.java
@@ -0,0 +1,13 @@
+package tut.sivex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingRequestsApplication.class, args);
+	}
+
+}
---
+++ TrackingRequests/src/test/java/tut/sivex/demo/TrackingRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package tut.sivex.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
